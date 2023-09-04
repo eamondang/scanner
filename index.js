92fc580c777fd2a0d35626e7a00f0ca85544630c
Eamon Đặng <eamondang@gmail.com>
2023-09-04 22:43:08 +0700

    fix: font

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ root.render(
         draggable
         pauseOnHover
         theme="dark"
-        style={{width: "800px", height: "1500px", fontSize:"80px"}}
+        style={{width: "800px", height: "3000px", fontSize:"80px", textAlign: "center"}}
       />
       <App />
     </BrowserRouter>
